33f9630d38122f9144af8d43edd8206a82a9ea1c
James Rees <James.rees@outlook.com>
2020-06-20 16:15:00 +0100

    Ensured that release and debug cmake targets link statically on OSX, there is a case sensitivity bug preventing VORBIS being discovered and linked that has been fixed on a later commit to master on the SFML branch so swapped to using a later commit from master

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,22 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    // while (window.isOpen())
-    // {
-    //     sf::Event event;
-    //     while (window.pollEvent(event))
-    //     {
-    //         if (event.type == sf::Event::Closed)
-    //             window.close();
-    //     }
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-    //     window.clear();
-    //     window.draw(shape);
-    //     window.display();
-    }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }    
 
     return 0;
 }
