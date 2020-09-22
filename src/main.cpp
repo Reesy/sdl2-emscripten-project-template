5ce2e3cb03d7f4bd1d019a5facf26eaaaedeccaf
James Rees <James.Rees@outlook.com>
2020-09-22 14:50:42 +0100

    Modified Cmake to not refer to removed tools

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,35 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
+// SDL2 Hello, World!
+// This should display a white screen for 2 seconds
+// compile with: clang++ main.cpp -o hello_sdl2 -lSDL2
+// run with: ./hello_sdl2
+#include <SDL.h>
+#include <stdio.h>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
 
-    sf::Music music;
-
-    if (!music.openFromFile("resources/music.wav"))
-    {
-        return -1;
-    }
-    
-    music.play();
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }    
-
-    return 0;
+int main(int argc, char* args[]) {
+  SDL_Window* window = NULL;
+  SDL_Surface* screenSurface = NULL;
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+    return 1;
+  }
+  window = SDL_CreateWindow(
+			    "hello_sdl2",
+			    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+			    SCREEN_WIDTH, SCREEN_HEIGHT,
+			    SDL_WINDOW_SHOWN
+			    );
+  if (window == NULL) {
+    fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+    return 1;
+  }
+  screenSurface = SDL_GetWindowSurface(window);
+  SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(2000);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
